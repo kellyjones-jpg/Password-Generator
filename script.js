d429b88a0aa27214c87e205dc84e5446e35c35c2
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2019-09-29 13:59:48 -0600

    more js changes

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ var strSpecial="!@#$%^&*()_-+=;:.?/";
 var strUpper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var strLower="abcdefghijklmnopqrstuvwxyz";
 var strCharType="";
-var cbxNumber="";
-var cbxSpecial="";
-var cbxUpper="";
-var cbxLower="";
 
 var iLength = prompt("Enter password length (must be between 8 and 128 characters:");
 alert(iLength);
 
-var type = window.confirm("Character types?")
+var type = confirm("Do you want to include special characters?");
+
+var numeric = confirm("Do you want to include numeric characters?");
+
+var lowercase = confirm("Do you want to include lowercase characters?");
+
+var uppercase = confirm("Do you want to include uppercase characters?");
+
+
