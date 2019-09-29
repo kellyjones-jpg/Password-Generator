62db2b447d40c35cc25ac99721546a1f3446f731
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2019-09-29 13:26:29 -0600

    updating js file

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var x="0123456789";
-var y="!@#$%^&*()_-+=;:.?/";
-var U="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var L="abcdefghijklmnopqrstuvwxyz";
+var strNumber="0123456789";
+var strSpecial="!@#$%^&*()_-+=;:.?/";
+var strUpper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var strLower="abcdefghijklmnopqrstuvwxyz";
 
 var length = prompt("Enter password length (must be between 8 and 128 characters:");
 alert(length);
 
 var type = window.confirm("Character types?")
-
