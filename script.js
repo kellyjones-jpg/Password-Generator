1fefb1e8f41a2e4ae7963ad8ef12ed3fabfca89b
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2019-09-29 21:28:38 -0600

    adjusting prompt again

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ var strUpper = "";
 var strLower = "";
 var strCharType = "";
 var temp = "";
+var x = 0;
 
 // Get password length
 while (x < 8 || x > 128)
 {
-var x = prompt("Enter password length (must be between 8 and 128 characters):");
+x = prompt("Enter password length (must be between 8 and 128 characters):");
 };
 
 // Select character types to use in password
