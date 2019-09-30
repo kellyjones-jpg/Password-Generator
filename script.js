a78e61841d19c59f158d4337f7b58c9fa201d2a5
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2019-09-29 18:40:46 -0600

    changing js

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
-var strNumber="";
-var strSpecial="";
-var strUpper="";
-var strLower="";
-var strCharType="";
-var temp="";
+var strNumber = "";
+var strSpecial = "";
+var strUpper = "";
+var strLower = "";
+var strCharType = "";
+var temp = "";
 
-var iPswdLength = prompt("Enter password length (must be between 8 and 128 characters:");
-alert(iPswdLength);
+var iPswdLength = prompt("Enter password length (must be between 8 and 128 characters):");
 
 while (strCharType.length === 0) {
     var answer = confirm("Do you want to include special characters?");
-    if (answer === true) {
-        strSpecial="!@#$%^&*()_-+=;:.?/"
+    if (answer === true) 
+    {
+        strSpecial = "!@#$%^&*()_-+=;:.?/"
     };
 
     answer = confirm("Do you want to include numeric characters?");
-    if (answer === true) {
-        strNumber="0123456789";
+    if (answer === true) 
+    {
+        strNumber = "0123456789";
     };
 
     answer = confirm("Do you want to include lowercase characters?");
-    if (answer === true) {
-        strLower="abcdefghijklmnopqrstuvwxyz";
+    if (answer === true) 
+    {
+        strLower = "abcdefghijklmnopqrstuvwxyz";
     };
 
     answer = confirm("Do you want to include uppercase characters?");
-    if (answer === true) {
-        strUpper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (answer === true) 
+    {
+        strUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     };
 
-    strCharType=strSpecial.concat(strNumber, strLower, strUpper);
+    strCharType = strSpecial.concat(strNumber, strLower, strUpper);
+
+    // "if your string character type . length = 0, put confirm saying'must select at least one character type'"
 };
 
-function generatePassword(iPswdLength){
-    temp="";
-    for (i=0;i<iPswdLength;i++) {
-    temp+=strCharType.charAt(Math.floor(Math.random()*strCharType.length))
-    return temp
-    };
-    };
+function generatePassword(iPswdLength) {
+    temp = "";
+    for (i = 0; i < iPswdLength; i++) {
+        temp += strCharType.charAt(Math.floor(Math.random() * strCharType.length))
+            };
+            return temp
+};
